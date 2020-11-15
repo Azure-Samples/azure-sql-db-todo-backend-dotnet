66b0b4aa0022b75bbdf91a33fc124dd294a710e1
Davide Mauri <damauri@microsoft.com>
2020-11-14 19:17:06 -0800

    added db scripts

--- ToDoBackEnd.Deploy/sql/001-create-db-objects.sql
+++ ToDoBackEnd.Deploy/sql/001-create-db-objects.sql
@@ -1,10 +1,10 @@
 create schema [web];
 go
 
-create user [todo-backend] with password = '$BackEndUserPassword$' -- substituted with reall password by DbUp
+create user [todo-backend] with password = '$BackEndUserPassword$' -- substituted with real password by DbUp
 go
 
-grant execute on schema::[web] to [todo-backend]
+grant execute on schema::[web] to [todo-backend];
 go
 
 create sequence dbo.[global_id]
@@ -16,7 +16,7 @@ go
 create table dbo.todos
 (
 	id int not null primary key default (next value for [global_id]),
-	todo nvarchar(100) not null,
+	title nvarchar(100) not null,
 	completed tinyint not null default (0)
 )
 go
